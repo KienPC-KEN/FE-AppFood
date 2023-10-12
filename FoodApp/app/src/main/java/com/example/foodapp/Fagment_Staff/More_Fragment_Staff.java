9eb785cd8bb8fc971324947847417cb5c134ffae
ThanhTung2803 <thanhtungg2803@gmail.com>
2023-10-12 15:35:07 +0700

    man hinh home

---
+++ FoodApp/app/src/main/java/com/example/foodapp/Fagment_Staff/More_Fragment_Staff.java
@@ -0,0 +1,6 @@
+package com.example.foodapp.Fagment_Staff;
+
+import androidx.fragment.app.Fragment;
+
+public class More_Fragment_Staff extends Fragment {
+}
--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,17 +1,54 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+
+import com.example.foodapp.Fagment_Staff.Menu_Fragment_Staff;
+import com.example.foodapp.Fagment_Staff.More_Fragment_Staff;
+import com.example.foodapp.Fagment_Staff.Oder_Fragment;
+import com.example.foodapp.Fagment_Staff.Recipt_Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText;
+        bottomNavigationView = findViewById(R.id.bottom_navigation_staff);
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.frameLayout, new Oder_Fragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        fm.beginTransaction().replace(R.id.frameLayout, new Oder_Fragment()).commit();
+                        return true;
+                    case R.id.menu:
+                        fm.beginTransaction().replace(R.id.frameLayout, new Menu_Fragment_Staff()).commit();
+                        return true;
+                    case R.id.history:
+                        fm.beginTransaction().replace(R.id.frameLayout, new Recipt_Fragment()).commit();
+                        return true;
+                    case R.id.more:
+                        fm.beginTransaction().replace(R.id.frameLayout, new More_Fragment_Staff()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
