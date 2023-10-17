d604e3b23a2931d7f8317cf266e7019db769bcd9
ThanhTam2909 <125848343+ThanhTam2909@users.noreply.github.com>
2023-10-17 17:13:18 +0700

    more

--- FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/More_Fragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/More_Fragment.java
@@ -1,65 +1,63 @@
 package com.example.foodapp.Fragment_Customer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.foodapp.Login.LoginActivity;
 import com.example.foodapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link More_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class More_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public More_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment More_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static More_Fragment newInstance(String param1, String param2) {
-        More_Fragment fragment = new More_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextView txtProfile, txtChangePass, txtLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more_customer, container, false);
+       View view = inflater.inflate(R.layout.fragment_more_customer, container, false);
+       txtProfile = view.findViewById(R.id.txt_profile_more);
+       txtChangePass = view.findViewById(R.id.txt_changePass);
+       txtLogout = view.findViewById(R.id.txt_logout);
+
+       txtProfile.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               FragmentManager manager = getActivity().getSupportFragmentManager();
+               FragmentTransaction transaction = manager.beginTransaction();
+               Profile_Fragment profileFragment = new Profile_Fragment();
+               transaction.add(R.id.frameLayout, profileFragment);
+               transaction.commit();
+           }
+       });
+
+       txtChangePass.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+               FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+               ChangePass_Fragment changePassFragment = new ChangePass_Fragment();
+               fragmentTransaction.replace(R.id.frameLayout, changePassFragment);
+               fragmentTransaction.commit();
+           }
+       });
+
+       txtLogout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getActivity(), LoginActivity.class);
+               startActivity(intent);
+           }
+       });
+       return view;
     }
 }
--- FoodApp/app/src/main/java/com/example/foodapp/Login/LoginFragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Login/LoginFragment.java
@@ -8,25 +8,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.foodapp.Customer_Activity;
 import com.example.foodapp.R;
 
 public class LoginFragment extends Fragment {
 
     private TextView forgotPass;
+    private Button btnLogin;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         forgotPass = view.findViewById(R.id.txt_forgotpass);
+        btnLogin = view.findViewById(R.id.login_btn);
 
 
         forgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Customer_Activity.class);
                 startActivity(intent);
             }
         });
