2cda1096e40647454c7b8befb65475d24d9127ec
KienPC-KEN <phungchikien196@gmail.com>
2023-10-23 00:02:04 +0700

    call api login

---
+++ FoodApp/app/src/main/java/com/example/foodapp/config/Config.java
@@ -0,0 +1,6 @@
+package com.example.foodapp.config;
+
+public class Config {
+    //Thay đổi địa chỉ IP thành địa chỉ IPv4 address của máy mình
+   public static final String IP = "192.122.23.102";
+}
