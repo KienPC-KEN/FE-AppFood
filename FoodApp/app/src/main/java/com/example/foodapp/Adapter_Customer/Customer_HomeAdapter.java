8e1a21a25b4060cb0b4cdfe02f6badfa70a8b6e6
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-10-31 09:33:50 +0700

    home adapter

---
+++ FoodApp/app/src/main/java/com/example/foodapp/Adapter_Customer/Customer_HomeAdapter.java
@@ -0,0 +1,57 @@
+package com.example.foodapp.Adapter_Customer;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodapp.Model.Product;
+import com.example.foodapp.databinding.ItemDrinkCustomerBinding;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Customer_HomeAdapter extends RecyclerView.Adapter<Customer_HomeAdapter.Holder> {
+    private final ArrayList<Product> list;
+
+    public Customer_HomeAdapter(ArrayList<Product> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Holder(ItemDrinkCustomerBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.bindData(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(list != null) {
+            return list.size();
+        }
+        return 0;
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder {
+
+        ItemDrinkCustomerBinding binding;
+
+        public Holder(@NonNull ItemDrinkCustomerBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bindData(Product product){
+            Picasso.get().load(product.getImage()).into(binding.imgDrink);
+            binding.tvNameDrink.setText(product.getName());
+            binding.tvPriceDrink.setText(product.getPrice().toString());
+        }
+    }
+}
