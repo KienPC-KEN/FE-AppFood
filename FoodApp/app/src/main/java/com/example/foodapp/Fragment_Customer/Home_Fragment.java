b4a5f4fc1e6c04e7a00c7e8a123dc6fc25e3961d
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-10-31 09:34:11 +0700

    call api product

--- FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/Home_Fragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/Home_Fragment.java
@@ -6,27 +6,42 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.foodapp.Adapter_Customer.Customer_HomeAdapter;
+import com.example.foodapp.Model.Product;
 import com.example.foodapp.R;
+import com.example.foodapp.config.Config;
+import com.example.foodapp.config.VolleySingleton;
+import com.example.foodapp.databinding.FragmentHomeCustomerBinding;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 
 public class Home_Fragment extends Fragment {
-
-
-
-    public Home_Fragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        Log.e("thanhtung","Fragment home");
-    }
+    private static final String TAG = "Home_Fragment";
+    private FragmentHomeCustomerBinding binding;
+    private ArrayList<Product> list;
+    private Customer_HomeAdapter adapter;
 
     //load lại dữ liệu fragment
     @Override
@@ -38,18 +53,34 @@ public class Home_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home_customer, container, false);
-
-        SearchView sv_foods = view.findViewById(R.id.sv_foods);
-        sv_foods.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sv_foods.setIconified(false);
-            }
-        });
-
-        return view;
+        binding = FragmentHomeCustomerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        list = new ArrayList<>();
+
+        JsonArrayRequest getProduct = new JsonArrayRequest(Request.Method.GET, Config.IP + "product", null,
+                response -> {
+                    list = new Gson().fromJson(response.toString(), new TypeToken<ArrayList<Product>>() {
+                    }.getType());
+                    adapter = new Customer_HomeAdapter(list);
+                    binding.recyclerViewDrinks.setAdapter(adapter);
+                    binding.recyclerViewFoods.setAdapter(adapter);
+                }
+                , error -> {
+                    Toast.makeText(getContext(), "something went wrong", Toast.LENGTH_SHORT).show();
+        });
+        VolleySingleton.getInstance(getContext()).addToRequestQueue(getProduct);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        binding = null;
+        super.onDestroy();
+    }
 }
