131111378659f2a00b92becc9e2860a733c219a3
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-11-02 18:16:15 +0700

    fix menu category adapter

--- FoodApp/app/src/main/java/com/example/foodapp/Adapter_Customer/Menu_CategoryAdapter.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Adapter_Customer/Menu_CategoryAdapter.java
@@ -1,19 +1,24 @@
 package com.example.foodapp.Adapter_Customer;
 
+import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodapp.Model.Category;
+import com.example.foodapp.Model.Product;
 import com.example.foodapp.databinding.ItemCategoryBinding;
 
 import java.util.ArrayList;
 
 public class Menu_CategoryAdapter extends RecyclerView.Adapter<Menu_CategoryAdapter.Holder> {
 
-    private ArrayList<Category> list;
+    private final ArrayList<Category> list;
 
     public Menu_CategoryAdapter(ArrayList<Category> list) {
         this.list = list;
@@ -28,6 +33,13 @@ public class Menu_CategoryAdapter extends RecyclerView.Adapter<Menu_CategoryAdap
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.bindData(list.get(position));
+        holder.binding.tvCategoryName.setOnClickListener(v -> {
+            if(holder.binding.tvCategoryName.getTypeface() == Typeface.DEFAULT) {
+                holder.binding.tvCategoryName.setTypeface(null, Typeface.BOLD);
+            }else {
+                holder.binding.tvCategoryName.setTypeface(Typeface.DEFAULT);
+            }
+        });
     }
 
     @Override
