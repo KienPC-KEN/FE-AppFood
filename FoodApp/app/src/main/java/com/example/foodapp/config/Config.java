2b66c8541568a9c8266047c0daf2fa2f6e12cfc5
KienPC-KEN <phungchikien196@gmail.com>
2023-11-10 16:41:51 +0700

    details profile

--- FoodApp/app/src/main/java/com/example/foodapp/config/Config.java
+++ FoodApp/app/src/main/java/com/example/foodapp/config/Config.java
@@ -2,5 +2,5 @@ package com.example.foodapp.config;
 
 public class Config {
     //Thay đổi địa chỉ IP thành địa chỉ IPv4 address của máy mình
-   public static final String IP = "http://192.168.0.103:3000/api/";
+   public static final String IP = "http://172.20.10.3:3000/api/";
 }
