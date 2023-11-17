998f5411ce1fecaa60b43e96621cfbccf6b02213
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-11-17 22:30:22 +0700

    call api get method

--- FoodApp/app/src/main/java/com/example/foodapp/Fagment_Staff/ManagerAccountFragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Fagment_Staff/ManagerAccountFragment.java
@@ -1,35 +1,26 @@
 package com.example.foodapp.Fagment_Staff;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.foodapp.Interface.Deletestaff;
 import com.example.foodapp.Model.Staff;
 import com.example.foodapp.R;
 import com.example.foodapp.adapter_staff.StaffAdapter;
 import com.example.foodapp.config.Config;
+import com.example.foodapp.config.VolleySingleton;
+import com.example.foodapp.databinding.FragmentManagerAccountBinding;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,147 +28,49 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ManagerAccountFragment extends Fragment implements StaffAdapter.Deletestaff {
+public class ManagerAccountFragment extends Fragment {
+    private static final String TAG = "ManagerAccountFragment";
+    public FragmentManagerAccountBinding binding;
+    public ArrayList<Staff> staffData;
+    public StaffAdapter adapter;
 
-    private TextView txtEdit;
-    private Button btnAddAcc;
-    private ImageView img_Back;
-
-    public List<Staff> staffList;
-
-    public RecyclerView recyclerView;
-
-
-    public StaffAdapter staffAdapter ;
-
-    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_manager_account, container, false);
-        txtEdit = view.findViewById(R.id.txt_editacc);
-        btnAddAcc = view.findViewById(R.id.btn_addcount);
-
-        img_Back = view.findViewById(R.id.img_BackManager);
-
-        staffList = new ArrayList<>();
-        staffAdapter = new StaffAdapter(staffList);
-
-        recyclerView = view.findViewById(R.id.rcystaff);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(staffAdapter);
-
-        getds(getContext());
-        staffAdapter.xoa(this);
-
-        img_Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.frame_Accounteditacc, new MoreFragment_Staff()).commit();
-            }
-        });
-
-        txtEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction =  fragmentManager.beginTransaction();
-                EditAccount_Staff_Fragment editAccountStaffFragment = new EditAccount_Staff_Fragment();
-                transaction.add(R.id.frame_Accounteditacc, editAccountStaffFragment);
-                transaction.commit();
-            }
-        });
-
-        btnAddAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager manager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction =  manager.beginTransaction();
-                fragmentTransaction.add(R.id.frame_Accounteditacc, new AddAccountSatff_Fragment());
-                fragmentTransaction.commit();
-            }
-        });
-
-        return view;
+        binding = FragmentManagerAccountBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
-    public  void  getds(Context context){
-        RequestQueue requestQueue = Volley.newRequestQueue(context);
-
-        StringRequest request = new StringRequest(Request.Method.GET, Config.IP + "staff", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-                try {
-                    JSONObject perObject = new JSONObject(response);
-                    JSONArray dataJson = perObject.getJSONArray("dataJson");
-                    for (int i =0;i< dataJson.length();i++){
-                        JSONObject jsonObject = dataJson.getJSONObject(i).getJSONObject("data");
-                        Staff staff = new Staff();
-                        staff.setName(jsonObject.getString("name"));
-                        staff.setSex(jsonObject.getString("sex"));
-                        JSONArray jsonArray = jsonObject.getJSONArray("staff");
-                        List<Staff.staff> list = new ArrayList<>();
-                        for (int a =0;a< jsonArray.length();a++){
-                            JSONObject jsonObject1 = jsonArray.getJSONObject(a);
-                            Staff.staff staff1 = new Staff.staff();
-                            staff1.setId(jsonObject1.getString("_id"));
-                            list.add(staff1);
-                        }
-                        staff.setStaff(list);
-                        staffList.add(staff);
-                    }
-                    staffAdapter.notifyDataSetChanged();
-
-                }catch (JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        requestQueue.add(request);
-    }
     @Override
-    public void delete(int position) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle("DELETE ?");
-        builder.setMessage("bạn có muốn xóa không ?");
-        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                xoa(position);
-            }
-        });
-        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-    }
-    public  void xoa(int position){
-        String url = Config.IP + "deleteStaff/" + staffList.get(position).getStaff().get(0).getId();
-        RequestQueue queue = Volley.newRequestQueue(getContext());
-        StringRequest request = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                staffList.remove(position);
-                staffAdapter.notifyItemRemoved(position);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-            }
-        });
-        queue.add(request);
+        binding.imgBack.setOnClickListener(v -> requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new MoreFragment_Staff()).commit());
+
+        staffData = new ArrayList<>();
+
+        getStaffData();
     }
+
+    public void getStaffData() {
+        VolleySingleton.getInstance(requireActivity()).addToRequestQueue(new StringRequest(Config.IP + "staff", response -> {
+            try {
+                String jsonArray = String.valueOf(new JSONObject(response).getJSONArray("listStaff"));
+                staffData = new Gson().fromJson(jsonArray, new TypeToken<ArrayList<Staff>>() {
+                }.getType());
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    List<Staff> newStaff = staffData.stream().filter(staff -> "staff".equals(staff.getRole())).collect(Collectors.toList());
+                    adapter = new StaffAdapter((ArrayList<Staff>) newStaff);
+                    binding.rcvStaff.setAdapter(adapter);
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }, error -> {
+            Toast.makeText(requireActivity(), "onFailure: " + error.networkResponse.statusCode, Toast.LENGTH_SHORT).show();
+        }));
+    }
+
 }
