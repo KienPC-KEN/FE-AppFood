31aef5aebf912b1fe7229ed4d32c1163431ed201
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-11-18 02:35:39 +0700

    fix something

--- FoodApp/app/src/main/java/com/example/foodapp/Fagment_Staff/ProfileFragment_Staff.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Fagment_Staff/ProfileFragment_Staff.java
@@ -6,65 +6,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
 import com.example.foodapp.R;
+import com.example.foodapp.databinding.FragmentProfileStaffBinding;
 
 public class ProfileFragment_Staff extends Fragment {
-    private ImageView imgBack, imgAVT;
-    private TextView txtName, txtPhone, txtDate, txtSex, txtEmail, txtAddress;
+    public FragmentProfileStaffBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile_staff, container, false);
-        imgBack = view.findViewById(R.id.back);
-        imgAVT = view.findViewById(R.id.imgAvt);
-
-        txtName = view.findViewById(R.id.nameStaff);
-        txtPhone = view.findViewById(R.id.phoneStaff);
-        txtDate = view.findViewById(R.id.dateStaff);
-        txtSex = view.findViewById(R.id.sexStaff);
-        txtEmail = view.findViewById(R.id.emailStaff);
-        txtAddress = view.findViewById(R.id.addressStaff);
-
-        getDataUserStaff();
-
-        imgBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                MoreFragment_Staff moreFragmentStaff = new MoreFragment_Staff();
-                transaction.replace(R.id.frameLayout, moreFragmentStaff);
-                transaction.commit();
-            }
-        });
-        return view;
+        binding = FragmentProfileStaffBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
     }
 
-    private void getDataUserStaff() {
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Profile", Context.MODE_PRIVATE);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.imgBack.setOnClickListener(v -> requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new MoreFragment_Staff()).commit());
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("Profile", Context.MODE_PRIVATE);
+        String id = sharedPreferences.getString("id", "");
         String name = sharedPreferences.getString("name", "");
         String phone = sharedPreferences.getString("phone", "");
         String date = sharedPreferences.getString("date", "");
-        String sex = sharedPreferences.getString("sex", "");
+        String gender = sharedPreferences.getString("sex", "");
         String email = sharedPreferences.getString("email", "");
         String address = sharedPreferences.getString("address", "");
         String image = sharedPreferences.getString("image", "");
 
-        txtName.setText(name);
-        txtPhone.setText(phone);
-        txtDate.setText(date);
-        txtSex.setText(sex);
-        txtEmail.setText(email);
-        txtAddress.setText(address);
+        getDataUserStaff(name, phone, date, gender, email, address, image);
 
-        Glide.with(getActivity()).load(image).centerCrop().into(imgAVT);
+        editChange(id);
+    }
+
+    private void getDataUserStaff(String name, String phone, String date, String gender, String email, String address, String image) {
+        binding.edtName.setText(name);
+        binding.edtPhone.setText(phone);
+        binding.edtDate.setText(date);
+        binding.edtGender.setText(gender);
+        binding.edtMail.setText(email);
+        binding.edtAddress.setText(address);
+        binding.edtImage.setText(image);
+
+        Glide.with(requireActivity()).load(image).centerCrop().into(binding.imgAvt);
+    }
+
+    public void editChange(String id) {
+        binding.editSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                binding.layoutName.setEnabled(true);
+                binding.layoutPhone.setEnabled(true);
+                binding.layoutDate.setEnabled(true);
+                binding.layoutGender.setEnabled(true);
+                binding.layoutMail.setEnabled(true);
+                binding.layoutAddress.setEnabled(true);
+                binding.layoutImage.setVisibility(View.VISIBLE);
+            } else {
+                binding.layoutName.setEnabled(false);
+                binding.layoutPhone.setEnabled(false);
+                binding.layoutDate.setEnabled(false);
+                binding.layoutGender.setEnabled(false);
+                binding.layoutMail.setEnabled(false);
+                binding.layoutAddress.setEnabled(false);
+                binding.layoutImage.setVisibility(View.GONE);
+            }
+        });
     }
 }
--- FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/Profile_Fragment.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Fragment_Customer/Profile_Fragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -15,62 +17,77 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.foodapp.Fagment_Staff.MoreFragment_Staff;
 import com.example.foodapp.R;
+import com.example.foodapp.databinding.FragmentProfileBinding;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class Profile_Fragment extends Fragment {
-
-    private ImageView img_Back;
-    private TextView tvName, tvPhone, tvDate, tvSex, tvEmail, tvAddress;
-
-    private CircleImageView imgAvatar;
-
+    public FragmentProfileBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile_, container, false);
-        img_Back = view.findViewById(R.id.img_backprofile);
-        tvName = view.findViewById(R.id.txt_NamePro);
-        tvPhone = view.findViewById(R.id.txt_PhonePro);
-        tvDate = view.findViewById(R.id.txt_DatePro);
-        tvSex = view.findViewById(R.id.txt_SexPro);
-        tvEmail = view.findViewById(R.id.txt_EmailPro);
-        tvAddress = view.findViewById(R.id.txt_AddressPro);
-        imgAvatar = view.findViewById(R.id.imagePro);
-        getDataUser();
-        img_Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.frameLayout, new More_Fragment()).commit();
-                Log.d("Back", "onClick: ");
-            }
-        });
+        binding = FragmentProfileBinding.inflate(getLayoutInflater(), container, false);
 
-        return view;
+        return binding.getRoot();
     }
 
-    private void getDataUser() {
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Profile", Context.MODE_PRIVATE);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.imgBack.setOnClickListener(v -> requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new More_Fragment()).commit());
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("Profile", Context.MODE_PRIVATE);
+        String id = sharedPreferences.getString("id", "");
         String name = sharedPreferences.getString("name", "");
         String phone = sharedPreferences.getString("phone", "");
         String date = sharedPreferences.getString("date", "");
-        String sex = sharedPreferences.getString("sex", "");
-        String image = sharedPreferences.getString("image", "");
+        String gender = sharedPreferences.getString("sex", "");
         String email = sharedPreferences.getString("email", "");
         String address = sharedPreferences.getString("address", "");
+        String image = sharedPreferences.getString("image", "");
 
-        tvName.setText(name);
-        tvPhone.setText(phone);
-        tvDate.setText(date);
-        tvSex.setText(sex);
-        tvEmail.setText(email);
-        tvAddress.setText(address);
+        getDataUserStaff(name, phone, date, gender, email, address, image);
 
-        Glide.with(getActivity()).load(image).centerCrop().into(imgAvatar);
+        editChange(id);
+    }
+
+    private void getDataUserStaff(String name, String phone, String date, String gender, String email, String address, String image) {
+        binding.edtName.setText(name);
+        binding.edtPhone.setText(phone);
+        binding.edtDate.setText(date);
+        binding.edtGender.setText(gender);
+        binding.edtMail.setText(email);
+        binding.edtAddress.setText(address);
+        binding.edtImage.setText(image);
+
+        Glide.with(requireActivity()).load(image).centerCrop().into(binding.imgAvt);
+    }
+
+    public void editChange(String id) {
+        binding.editSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                binding.layoutName.setEnabled(true);
+                binding.layoutPhone.setEnabled(true);
+                binding.layoutDate.setEnabled(true);
+                binding.layoutGender.setEnabled(true);
+                binding.layoutMail.setEnabled(true);
+                binding.layoutAddress.setEnabled(true);
+                binding.layoutImage.setVisibility(View.VISIBLE);
+            } else {
+                binding.layoutName.setEnabled(false);
+                binding.layoutPhone.setEnabled(false);
+                binding.layoutDate.setEnabled(false);
+                binding.layoutGender.setEnabled(false);
+                binding.layoutMail.setEnabled(false);
+                binding.layoutAddress.setEnabled(false);
+                binding.layoutImage.setVisibility(View.GONE);
+            }
+        });
     }
 }
