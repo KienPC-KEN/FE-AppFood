0cd0e8845eb5c01ede585723e1f0683b569f5874
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-11-18 02:34:29 +0700

    fix something

--- FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import com.example.foodapp.Fagment_Staff.Menu_Fragment_Staff;
 import com.example.foodapp.Fagment_Staff.MoreFragment_Staff;
 import com.example.foodapp.Fagment_Staff.Home_Fragment;
 import com.example.foodapp.Fagment_Staff.StaffCategoryFragment;
-import com.example.foodapp.Util.BottomNavigationViewBehavior;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction().replace(R.id.frameLayout, new Home_Fragment()).commit();
 
-        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
-        layoutParams.setBehavior(new BottomNavigationViewBehavior());
         bottomNavigationView.getMenu().findItem(R.id.history).setVisible(false);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
